5e50586ef7718b39ff0eea94b58dc7b67d397822
Vsevolod <vje13@outlook.com>
2022-07-07 16:26:59 +0300

    small commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// single line comment
+
+/*
+    multi
+    line
+    comment
+*/
+
+console.log(1);     // comment on the side
+console.log(2);
+
+// above code
+console.log(3);
+
+// another comment
+console.log(4);
